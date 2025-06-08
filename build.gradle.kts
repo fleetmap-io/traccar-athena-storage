b885051e9d62ed92c0ee47b8f1c932bf896cbd54
joaquimcardeira <joaquim.cardeira@gmail.com>
2025-06-08 21:31:19 +0100

    download traccar

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
+import de.undercouch.gradle.tasks.download.Download
+import java.io.File
+
 plugins {
     id("java")
+    id("de.undercouch.download") version "5.4.0"
 }
 
 group = "org.fleetmap"
@@ -7,13 +11,37 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    flatDir {
+        dirs("libs")
+    }
 }
 
 dependencies {
+    implementation(":tracker-server")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+val traccarZip = "https://github.com/traccar/traccar/releases/download/v6.7.2/traccar-other-6.7.2.zip"
+val traccarZipFile = File(buildDir, "traccar-other-6.7.2.zip")
+val traccarLibDir = file("libs")
+
+tasks.register<Download>("downloadTraccar") {
+    src(traccarZip)
+    dest(traccarZipFile)
+    overwrite(false)
+}
+
+tasks.register<Copy>("unzipTraccar") {
+    dependsOn("downloadTraccar")
+    from(zipTree(traccarZipFile))
+    into(traccarLibDir)
+}
+
+tasks.named("compileJava") {
+    dependsOn("unzipTraccar")
 }
